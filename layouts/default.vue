e80ae24e96cc0ae56e9d77df338b691b7bc5aca2
Guen <heloo@guen.dev>
2023-03-11 22:26:47 +0700

    cm

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,9 @@
   <n-layout position="absolute">
     <n-layout-header style="height: 64px; padding:0 24px" bordered>
       <div class="flex items-center h-full">
-        <h1 class="w-[216px] flex-shrink-0">Financial 247</h1>
+        <h1 class="w-[216px] flex-shrink-0">
+          Bill Creator
+        </h1>
         <div id="tabs" class="w-full h-full flex items-center"></div>
         <div id="actions" class="flex-shrink-0"></div>
       </div>
@@ -27,16 +29,7 @@
 </template>
 
 <script lang="ts" setup>
-import {GlobalThemeOverrides} from "naive-ui/es/config-provider/src/interface";
-
 const [collapsed, toggleCollapsed] = useToggle(false)
-
-
-const themeOverrides = reactive<GlobalThemeOverrides>({
-  common: {
-    primaryColor: '#1890ff'
-  }
-})
 </script>
 
 <style scoped></style>
